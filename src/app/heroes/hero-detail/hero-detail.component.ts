fc433aabd5d99e977ca040cef0e10f4e9532e5c2
loktionov129 <loktionov129@gmail.com>
2017-08-20 16:20:19 +0700

    [hotfix] handle not found hero-detail id

--- src/app/heroes/hero-detail/hero-detail.component.ts
+++ src/app/heroes/hero-detail/hero-detail.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { ActivatedRoute, ParamMap } from '@angular/router';
+import { ActivatedRoute, ParamMap, Router } from '@angular/router';
 import { Location } from '@angular/common';
 
 import { Hero } from '../hero';
@@ -20,12 +20,13 @@ export class HeroDetailComponent implements OnInit, OnDestroy, ComponentCanDeact
   constructor(
     private heroService: HeroService,
     private route: ActivatedRoute,
-    private location: Location
+    private location: Location,
+    private router: Router
   ) {}
 
   public canDeactivate(): boolean | Observable<boolean> {
 
-    if (this.hero.name) {
+    if (!this.hero || this.hero.name) {
       return true;
     } else {
       return confirm('Вы хотите покинуть страницу?');
@@ -35,7 +36,7 @@ export class HeroDetailComponent implements OnInit, OnDestroy, ComponentCanDeact
   public ngOnInit(): void {
     this.data$ = this.route.paramMap
       .switchMap((params: ParamMap) => this.heroService.getHero(+params.get('id')))
-      .subscribe((hero: Hero) => this.hero = hero);
+      .subscribe((hero: Hero) => this.hero = hero, (err) => this.router.navigate(['/heroes']));
   }
 
   public ngOnDestroy(): void {
